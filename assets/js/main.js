7455210cee3ae11803b103f5c67829aac8120ae8
Emiliany Fiirst <85531168+emilyfiirst@users.noreply.github.com>
2023-01-05 07:54:26 -0400

    aaa

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,14 +8,15 @@ function convertPokemonToLi(pokemon){
     `<li class="pokemon">
         <span class="number">#001</span>
         <span class="name">${pokemon.name}</span>
-            <div class="detail">
-                <ol class="types">
-                    <li class="type">grass</li>
-                    <li class="type">poison</li>
-                </ol>
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
-            </div>
-    </li>` 
+        <div class="detail">
+            <ol class="types">
+                <li class="type">grass</li>
+                <li class="type">poison</li>
+            </ol>
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+        </div>
+    </li>
+    `
 }
 
 const pokemonList = document.getElementById('pokemonList');
